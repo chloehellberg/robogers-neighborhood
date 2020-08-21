5030bb773a1f911593ec36cd819a31557afc0a24
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-21 11:11:59 -0700

    Second spec run and passed test - Beep! will push if i is 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-let number = 4;                           
+// let number = 4;                           
 
-function numberList(number) {              
+// function numberList(number) {              
+//   let roboArray = [];
+//   for (let i=0; i <= number; i+=1) {
+//     roboArray.push(i);             
+//   };
+//   return roboArray;
+// }
+// const result = numberList(number);
+// console.log(result);
+
+
+
+let number = 4;
+
+function numberList(number) {
   let roboArray = [];
   for (let i=0; i <= number; i+=1) {
-    roboArray.push(i);             
+    
+    if (i === 1) {
+      roboArray.push("Beep!");
+    }
+    else {
+      roboArray.push(i);
+    }
   };
   return roboArray;
-}
+};
 const result = numberList(number);
 console.log(result);
-
-
-
 
 
 

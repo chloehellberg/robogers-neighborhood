9c605310d76062113aafdb7bd9481012e5e78700
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-21 11:40:45 -0700

    Spec 3 is running but pushing 0 off of the array. Trying to determine why it is doing this

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-// SPEC #1
-// let number = 4;                           
+SPEC #1
+let number = 4;                           
 
-// function numberList(number) {              
-//   let roboArray = [];
-//   for (let i=0; i <= number; i+=1) {
-//     roboArray.push(i);             
-//   };
-//   return roboArray;
-// }
-// const result = numberList(number);
-// console.log(result);
+function numberList(number) {              
+  let roboArray = [];
+  for (let i=0; i <= number; i+=1) {
+    roboArray.push(i);             
+  };
+  return roboArray;
+}
+const result = numberList(number);
+console.log(result);
 
 SPEC #2
 let number = 4;
@@ -17,11 +17,9 @@ let number = 4;
 function numberList(number) {
   let roboArray = [];
   for (let i=0; i <= number; i+=1) {
-    
     if (i === 1) {
       roboArray.push("Beep!");
-    }
-    else {
+    } else {
       roboArray.push(i);
     }
   };
@@ -29,6 +27,27 @@ function numberList(number) {
 };
 const result = numberList(number);
 console.log(result);
+
+
+SPEC #3
+let number = 4;
+
+function numberList(number) {
+  let roboArray = [];
+  for (i=1; i <= number; i+=1) {
+    if (i === 1) {
+      roboArray.push("Beep!");
+    } else if (i === 2) {
+      roboArray.push("Boop!");
+    } else {
+      roboArray.push(i);
+    }
+  };
+  return roboArray;
+};
+const result = numberList(number);
+console.log(result);
+
 
 
 

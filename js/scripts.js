dd2dd7a00b622401caa9e8c7e7d13a4db7e148c2
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-21 15:01:35 -0700

    Separate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,5 @@
-// SPEC #1
-let number = 4;                           
-function numberList(number) {              
-  let roboArray = [];
-  for (let i=0; i <= number; i+=1) {
-    roboArray.push(i);             
-  };
-  return roboArray;
-}
-const result = numberList(number);
-console.log(result);
+// Business Logic
 
-// SPEC #2
-let number = 4;
-function numberList(number) {
-  let roboArray = [];
-  for (let i=0; i <= number; i+=1) {
-    if (i === 1) {
-      roboArray.push("Beep!");
-    } else {
-      roboArray.push(i);
-    }
-  };
-  return roboArray;
-};
-const result = numberList(number);
-console.log(result);
-
-
-// SPEC #3
-let number = 4;
-function numberList(number) {
-  let roboArray = [];
-  for (i=0; i <= number; i+=1) {
-    if (i === 1) {
-      roboArray.push("Beep!");
-    } else if (i === 2) {
-      roboArray.push("Boop!");
-    } else {
-      roboArray.push(i);
-    }
-  };
-  return roboArray;
-};
-const result = numberList(number);
-console.log(result);
-
-// SPEC #4
-let number = 4;
-function numberList(number) {
-  let roboArray=[];
-  for(i=0; i <= number; i +=1) {
-    if (i === 3) {
-      roboArray.push("Won't you be my neighbor?");
-    } else if (i === 2) {
-      roboArray.push("Boop!");
-    } else if (i === 1) {
-      roboArray.push("Beep!");
-    } else {
-      roboArray.push(i);
-    }
-  };
-  return roboArray;
-};
-const result = numberList(number);
-console.log(result);
-
-
-// SPEC #5
-let number = 23;
 function numberList(number) {
   let roboArray=[];
   
@@ -86,5 +18,19 @@ function numberList(number) {
   };
   return roboArray;
 };
-const result = numberList(number);
-console.log(result);
+
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("#neighborhood").submit(function(event) {
+    event.preventDefault();
+
+    const number = parseInt($('input#number').val());
+    const result = numberList(number);
+
+    $("#output").text(result);
+  });
+});
+
+
